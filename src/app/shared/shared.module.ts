61a3c455513fd68742e4a2b67d1756935c29b24d
ConxiCr <conxicr@hotmail.com>
2021-07-09 00:30:47 +0200

    creación errorPage página 404
    Signed-off-by: ConxiCr <conxicr@hotmail.com>

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ErrorPageComponent } from './error-page/error-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    ErrorPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
