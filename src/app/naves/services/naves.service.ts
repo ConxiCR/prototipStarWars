f71f0c0ea0d76d6fec7dc3ee2eea59a64c23b767
ConxiCr <conxicr@hotmail.com>
2021-07-10 08:04:39 +0200

    variable de entorno.Environment
    Signed-off-by: ConxiCr <conxicr@hotmail.com>

--- src/app/naves/services/naves.service.ts
+++ src/app/naves/services/naves.service.ts
@@ -3,15 +3,18 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Nave } from '../interfaces/naves.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NavesService {
 
+  private baseUrl: string = environment.baseUrl;
+
   constructor( private http: HttpClient ) { }
 
   getNaves(): Observable<Nave[]>{
-    return this.http.get<Nave[]>('https://swapi.dev/api/vehicles')
+    return this.http.get<Nave[]>(`${this.baseUrl}/vehicles/`)
   }
 }
